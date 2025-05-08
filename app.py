0c2866292a76340e9f5c461a0da6464e35e84e42
ContactCiss <richard@contactons.nl>
2025-05-08 09:50:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from email.message import EmailMessage
 app = Flask(__name__)
 
 SMTP_SERVER = os.getenv("SMTP_SERVER")
-SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
+SMTP_PORT = int(os.getenv("SMTP_PORT", "465"))  # SSL
 SMTP_USERNAME = os.getenv("SMTP_USERNAME")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 TO_EMAIL = os.getenv("TO_EMAIL")
@@ -32,8 +32,7 @@ def send_email(subject, body):
     msg['To'] = TO_EMAIL
     msg.set_content(body)
 
-    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-        server.starttls()
+    with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as server:
         server.login(SMTP_USERNAME, SMTP_PASSWORD)
         server.send_message(msg)
 
